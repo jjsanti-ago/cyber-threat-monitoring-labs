5e8f43cd8692af177a72eb96128deeb6fef05791
Justin Joy <justinejoycesantiago@gmail.com>
2026-08-25 15:52:05 +0800

    password criteria validation

--- projects/password-strength-checker/projects/password-strength-checker/PasswordStrengthChecker.java
+++ projects/password-strength-checker/projects/password-strength-checker/PasswordStrengthChecker.java
@@ -9,5 +9,39 @@ public class PasswordStrengthChecker {
         System.out.print("Enter password: ");
         String password = scanner.nextLine();
 
+        boolean hasUppercase = false;
+        boolean hasLowercase = false;
+        boolean hasNumber = false;
+        boolean hasSpecialCharacter = false;
+
+        for (char character : password.toCharArray()) {
+
+            if (Character.isUpperCase(character)) {
+                hasUppercase = true;
+            }
+
+            if (Character.isLowerCase(character)) {
+                hasLowercase = true;
+            }
+
+            if (Character.isDigit(character)) {
+                hasNumber = true;
+            }
+
+            if (!Character.isLetterOrDigit(character)) {
+                hasSpecialCharacter = true;
+            }
+        }
+
+        System.out.println("\nPassword Analysis");
+        System.out.println("-----------------");
+
+        System.out.println("Length: " + password.length());
+        System.out.println("Uppercase: " + hasUppercase);
+        System.out.println("Lowercase: " + hasLowercase);
+        System.out.println("Number: " + hasNumber);
+        System.out.println("Special Character: " + hasSpecialCharacter);
+
+        scanner.close();
     }
 }
