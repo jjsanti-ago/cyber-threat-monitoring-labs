16b69a9821fbefc0e5542ae19bd546e47c36ace7
Justin Joy <justinejoycesantiago@gmail.com>
2026-08-25 15:49:34 +0800

    Java project structure and user input

---
+++ projects/password-strength-checker/projects/password-strength-checker/PasswordStrengthChecker.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class PasswordStrengthChecker {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter password: ");
+        String password = scanner.nextLine();
+
+    }
+}
